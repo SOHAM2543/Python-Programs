0aa67edb8d20c6d0a2624231ef9d74fc5d0e5c75
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2023-09-15 10:05:24 +0530

    Add files via upload

---
+++ P1 Adding two numbers.py
@@ -0,0 +1,14 @@
+#solution-1 (With pre-defines variables)
+#num1 = 30
+#num2 = 50
+#sum = num1+num2
+#print("The sum of given two number is", sum)
+
+
+
+num1 = float(input("Enter a number here: "))
+num2 = float(input("Enter a Another number here: "))
+
+sum = num1 + num2
+
+print("The sum of provided two numbers is:", sum)
